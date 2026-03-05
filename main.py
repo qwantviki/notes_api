d892b368b6028d75101ba82915bc9ea93aaf8f46
qwantviki <qwantviki@gmail.com>
2026-03-05 12:16:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 from fastapi import FastAPI
 from models import Notebook
+from pydantic import BaseModel
+
+class NoteIn(BaseModel):
+    title: str
+    content: str
+    author: str
 
 app = FastAPI(title="Notes API")
 notebook = Notebook()
+
+@app.get("/notes")
+def get_notes():
+    """
+    Возвращает список всех заметок
+    """
+    notes = notebook.get_all_notes()
+    return [note.to_dict() for note in notes]
+
+@app.post("/notes")
+def create_note(note_data: NoteIn):
+    """
+    Создание новой заметки
+    """
+    note = notebook.add_note(
+        title=note_data.title,
+        content=note_data.content,
+        author=note_data.author
+    )
+    if note is None:
+        return {"error": "Note is not valid (title/author empty or content too long)"}
+    return note.to_dict()
